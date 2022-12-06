0557374a4ca62f9f2eab8b4c8d8eee5480694065
Okechukwu <okei.muokwugwo@outlook.com>
2022-12-06 21:30:34 +0100

    Update schema.sql with create table sql command prompt

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
 );
+
