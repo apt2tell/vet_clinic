89e57259e3a43e340069cc42a417158b9d44f59b
Okechukwu Muokwugwo <okei.muokwugwo@gmail.com>
2022-12-09 18:43:28 -0800

    Update schema.sql with create owners table

--- schema.sql
+++ schema.sql
@@ -13,3 +13,26 @@ CREATE TABLE IF NOT EXISTS public.animals
 
 ALTER TABLE animals ADD species VARCHAR(50)
 
+/* Create owners table */
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT
+);
+
+/*Create Species table */
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+/*Constraints */
+ALTER TABLE animals 
+ADD COLUMN species_id INT CONSTRAINT species_animals_fk 
+REFERENCES species(id)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INT CONSTRAINT owners_animals_fk 
+REFERENCES owners(id) 
+ON UPDATE CASCADE ON DELETE CASCADE;
